1d2cced9098de6d0db6a29ceed7b404dd6c95fe5
Maximiliano <maxiarielmorales712@gmail.com>
2025-09-03 17:31:02 -0300

    "Curso de JavaScript, hasta la hora de 1:22:20"
    "Curso de JavaScript, hasta la hora de 1:22:20"

---
+++ Basic/02-datatypes.js
@@ -0,0 +1,50 @@
+// Tipos de datos primitivos
+
+// Cadena de texto (String)
+let myName = "Maxi Morales"
+let alias = 'Max'
+let email = `maxiarielmorales712@gmail.com`
+
+// números (number)
+let age = 29 // Entero
+let height = 1.62 // Decimal
+
+// Booleanos (boolean)
+let isStudent = true
+let isTeacher = false
+
+// Undefined
+let undefinedValue
+console.log(undefinedValue)
+
+// Null
+let nullValue= null
+
+// Symbol
+
+let mySymbol = Symbol("mysymbol")
+
+// BigInt
+
+let myBigInt = BigInt(732894794837284723987492384)
+let myBigInt2 = 89472389472894728937489274892748974892374892374897829374283974n
+
+// Mostramos los tupos de datos
+console.log(typeof myName)
+console.log(typeof alias)
+console.log(typeof email)
+
+console.log(typeof age)
+console.log(typeof height)
+
+console.log(typeof isTeacher)
+console.log(typeof isStudent)
+
+console.log(typeof undefinedValue)
+
+console.log(typeof nullValue)
+
+console.log(typeof mySymbol)
+
+console.log(typeof myBigInt)
+console.log(typeof myBigInt2)
