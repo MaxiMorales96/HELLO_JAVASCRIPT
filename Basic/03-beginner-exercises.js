0e2e10d8980e3e5b838cb539e3ea296f41dbb73c
Maximiliano <maxiarielmorales712@gmail.com>
2025-09-03 17:38:32 -0300

    Hice dos EJercicios de practica
    Hice dos ejercicios de practica para el curso de JavaScript.

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -1,7 +1,11 @@
-// 1. EScribe un comentario en una linea
-
+// 1. Escribe un comentario en una linea
+// 1. Acá finaliza hasta datypes.js en 1:22:00
 // 2. Escribe un comentario en varias lineas
-
+/*
+Me encanta este curso de JavaScript para principiantes,
+tengo que realizar unos ejercicios para practicar yo solo.
+Desde comienzo del video, hasta 1:22:20, en youtube.
+*/
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
 
 // 4. Imprime por consola el valor de todas las variables
